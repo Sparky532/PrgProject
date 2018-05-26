22494d48ecea0f245b4f13c752b426b54c54f5e9
BerNd - TapLop <bernd.e.gevers@gmail.com>
2018-05-26 10:57:52 +0200

    File handler added

---
+++ DAL/FileHandler.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class FileHandler
+    {
+        string filePath = "";
+        private FileStream stream;
+        private StreamWriter writer;
+        private StreamReader reader;
+
+        public FileHandler(string fileParam = "SpeciesSuggestions.txt")
+        {
+            this.filePath = fileParam;
+        }
+
+        public void WriteData(string species)
+        {
+            try
+            {
+                if (!(File.Exists(filePath)))
+                {
+                    stream = new FileStream(filePath, FileMode.Create, FileAccess.Write);
+                }
+                stream = new FileStream(filePath, FileMode.Append, FileAccess.Write);
+                writer = new StreamWriter(stream);
+                writer.WriteLine(species);
+                writer.Flush();
+            }
+            catch (FileNotFoundException)
+            {
+                File.Create(filePath);
+            }
+            catch (DirectoryNotFoundException)
+            {
+                Directory.CreateDirectory(filePath);
+            }
+            catch (IOException)
+            {
+                File.Create(filePath);
+                File.AppendText(filePath);
+            }
+            finally
+            {
+                writer.Close();
+                stream.Close();
+            }
+        }
+    }
+}
