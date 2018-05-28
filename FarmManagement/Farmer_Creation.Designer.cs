b2a836e03964d16057df9d50f9072578c009bdfa
Megan Venter <meganventer11@gmail.com>
2018-05-28 18:30:36 +0200

    Unknown

--- FarmManagement/Farmer_Creation.Designer.cs
+++ FarmManagement/Farmer_Creation.Designer.cs
@@ -317,7 +317,7 @@
             // pictureBox1
             // 
             this.pictureBox1.Image = global::FarmManagement.Properties.Resources.FarmerCreation;
-            this.pictureBox1.Location = new System.Drawing.Point(435, 0);
+            this.pictureBox1.Location = new System.Drawing.Point(445, 0);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(785, 657);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
